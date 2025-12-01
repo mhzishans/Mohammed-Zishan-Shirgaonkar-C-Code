8024befe52c721c9e5723c56ecc1f030268fdd98
mhzishans <mhzishans@eng.rizvi.edu.in>
2025-12-01 15:24:03 +0530

    Update and rename EXP1.c to EXP-1-TASK1.c

---
+++ EXP-1-TASK1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() { 
+printf("Name: Mohammed Zishan Rizwan Shirgaonkar\n");
+printf("Age: 18\n"); 
+printf("Class: FE AI-DS\n"); 
+printf("Division : A\n"); 
+printf("UIN: 251A015\n"); 
+return 0;
+ }
+
